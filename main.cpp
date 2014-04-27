f63250d304d2d81c62263221c9a7e2ee5eb2e362
Marcel Windpassinger <smarcelw@gmail.com>
2014-04-27 09:28:27 +0200

    Add curve for visual representation

--- main.cpp
+++ main.cpp
@@ -8,7 +8,13 @@ int main(int argc, char *argv[])
     GraphPlot G(QSize(800,320));
     GraphCurve C("test");
     G.addGraphCurve(&C);
-    G.appendToGraphCurve(QPointF(50,75),"test");
+    G.appendToGraphCurve(QPointF(0,40),"test");
+    G.appendToGraphCurve(QPointF(10,22),"test");
+    G.appendToGraphCurve(QPointF(20,37),"test");
+    G.appendToGraphCurve(QPointF(30,13),"test");
+    G.appendToGraphCurve(QPointF(40,49),"test");
+    G.appendToGraphCurve(QPointF(50,33),"test");
+    G.appendToGraphCurve(QPointF(60,45),"test");
     G.show();
     return a.exec();
 }
